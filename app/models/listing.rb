d7a03697c65ba6c0319d6f96e31daa6b2a226557
shotop <shotop@thoughtworks.com>
2015-01-11 18:48:19 -0600

    shotop: add validations for users and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,4 +9,8 @@ class Listing < ActiveRecord::Base
   end
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :image, :attachment_presence => true
 end
